bd659c56029cdec1311b26900f56d785a2f7922a
jodyb22 <jody22brown@gmail.com>
2024-12-17 19:42:30 -0500

    complete excercise 6

---
+++ Module 1/lab6.js
@@ -0,0 +1,19 @@
+const book = {
+    title: 'The Wizards of Waverly Place'
+    author: 'Selana Gomez'
+    description: 'A book about kid wizards learning how to be a pro wizard'
+    numPages: 500
+}
+
+console.log (book.title);
+console.log (book.author);
+console.log (book.description);
+console.log (book.numpages);
+
+console.log(book)
+
+book.description= 'Wizards that grow as characaters into wizards'
+console.log(book.description)
+
+const books = [book, title, author, description, numPages]
+console.log(books);
